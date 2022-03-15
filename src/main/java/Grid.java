2db97ded7478c1fbe62a4fb7c0aaf8e573e6ca71
Mohamed Ahouzi <ahouzimohamed94@gmail.com>
2022-03-15 13:48:26 +0100

    step 3 done

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,10 +1,19 @@
 public class Grid {
-    private String[][] matrix;
+    private final String[][] matrix;
+    public Grid(){
+        matrix = new String[6][7];
+        for (int i=0;i<6;i++)
+            for (int j=0;j<7;j++)
+                matrix[i][j] = ".";
+    }
     public int getLines(){
         return 6;
     }
 
     public int getColumns() {
         return 7;
+    }
+    public String getToken(int row, int column) {
+        return matrix[row - 1][column - 1];
     }
 }
--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,17 +1,35 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GridTest {
+    Grid grid;
+    @BeforeEach
+    public void setUp(){
+        grid = new Grid();
+    }
     @Test
     public void shouldHave6LinesWhenInitializedGrid(){
-        Grid grid = new Grid();
+
         int result = grid.getLines();
-        Assertions.assertEquals(6,result);
+        assertEquals(6,result);
     }
     @Test
     public void shouldHave7ColumnsWhenInitializedGrid(){
-        Grid grid = new Grid();
         int result = grid.getColumns();
-        Assertions.assertEquals(7,result);
+        assertEquals(7,result);
+    }
+
+    @Test
+    public void gridShouldBeInitializedWithPoints(){
+       for (int i=0;i<6;i++) {
+           for (int j = 0; j < 7; j++) {
+               String result = grid.getToken(i + 1, j + 1);
+               assertEquals(".",result);
+           }
+       }
+
     }
 }
